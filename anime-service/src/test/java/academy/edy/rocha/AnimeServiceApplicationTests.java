747754c27fdf3624c397324827ede4ced4e4b1ac
edyrocha <rocha.eder.dev@gmail.com>
2024-12-03 22:17:47 +0000

    video03: initial commit of the project

---
+++ anime-service/src/test/java/academy/edy/rocha/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.edy.rocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
