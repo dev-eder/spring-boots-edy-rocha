ef1600c41e0edea8b6bf7f38ca18205e1060046a
edyrocha <rocha.eder.dev@gmail.com>
2024-12-04 22:54:06 +0000

    video03: initial commit of the project

---
+++ animes-services/src/main/java/com/edy/rocha/dev/AnimesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.edy.rocha.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimesServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimesServicesApplication.class, args);
+    }
+
+}
---
+++ animes-services/src/test/java/com/edy/rocha/dev/AnimesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edy.rocha.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
